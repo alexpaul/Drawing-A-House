814965d8e9397deaa15f56311912019e42a7e184
Alex Paul <axp_alpaul@yahoo.com>
2013-11-21 23:44:00 -0500

    Completed UIBezierPath Challenge

---
+++ HouseView.h
@@ -0,0 +1,13 @@
+//
+//  HouseView.h
+//  Drawing a House.
+//
+//  Created by Alex Paul on 11/21/13.
+//  Copyright (c) 2013 Alex Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HouseView : UIView
+
+@end
